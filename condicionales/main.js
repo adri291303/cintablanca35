63d967543b180880db90dd7a16a1307a7be9c7c4
adri130329 <adri_29.a@outlook.com>
2019-10-29 21:50:30 -0600

    commit

---
+++ condicionales/main.js
@@ -0,0 +1,87 @@
+//if(condicion){
+  //  comandos...
+//} else if(condicion2){
+//Comandos...
+//}
+//let edad= 18;
+
+//if (edad === 18) {
+  //  console.log( Eres mayor de edad );
+//} else {
+//  console.log( 'No eres mayor de edad);
+//}
+
+//let numero = Number(prompt("ingresa tu numero"))
+
+//if( numero % 2 === 0 ){
+//console.log(numero + "es un numero par")
+//}else{
+//    console.log(numero + "es un numero impar")
+//}
+
+// let dia = 0
+// if (dia === 0){
+//     console.log('lunes');
+// } else if(dia === 1) 
+//    {console.log('martes');
+// } else if(dia === 2) 
+//    {console.log('miercoles');
+// } else if(dia === 3) 
+//    {console.log('jueves');
+// } else if(dia === 4) 
+//    {console.log('viernes');
+// } else if(dia === 5) 
+//    {console.log('sabado');
+// } else if(dia === 6) 
+//    {console.log('domingo');
+// } else {
+//     console.log('dia invalido')
+// } 
+
+// let edad = prompt ("ingresa tu edad")
+// if ( edad <= 15){
+// console.log("no puedes conducir por ser menor de edad")
+// }else if(edad == 16 || edad == 17 ){
+// console.log("puedes sacar tu permiso parea conducir")
+// }else if(edad >= 18 && edad <=65 ){
+// console.log("puedes conducir")
+// }else if(edad > 65 ){
+//     console.log("no puedes conducir")
+// }else{
+//     console.log("no te entendi perro ")
+// }
+
+// let jugador1 = prompt ("ingresa tu jugada")
+// let jugador2 = prompt ("ingresa tu jugada")
+
+// let piedra = "piedra"
+// let papel = "papel"
+// let tijera = "tijera"
+
+// if(jugador1 === piedra && jugador2 === papel){
+//    console.log("gana jugador 2 porque eligio papel") 
+// }else if(jugador1 === papel && jugador2 === tijera){
+//     console.log("gana jugador 2 porque eligio tijera")
+// }else if(jugador1 === tijera && jugador2 === piedra){
+//     console.log("gana jugador 2 porque eligio piedra")
+// }else if(jugador1 === papel && jugador2 === piedra){
+//     console.log("gana jugador 1")
+// }else if(jugador1 === tijera && jugador2 === papel){
+//     console.log("gana jugador 1")
+// }else if(jugador1 === piedra && jugador2 === tijera){
+//     console.log("gana jugador 1")
+// }else if(jugador1 === piedra && jugador2 === piedra){
+//     console.log("es empate")
+// }else if(jugador1 === papel && jugador2 === papel){
+//     console.log("es empate")
+// }else if(jugador1 === tijera && jugador2 === tijera){
+//     console.log(" es empate")
+// }
+
+let pasword = prompt ("ingresa contraseña")
+
+  if(pasword === "perritoSalchicha123"){
+      console.log("bienvenido")
+  } else{
+      console.error("denegado")
+  }
